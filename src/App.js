9c27da6b0dc9f04c8442445f1a28a3bea31d44a6
Dmitro <dimamar1003@gmail.com>
2022-09-03 23:14:50 +0100

    Created first component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React, {useState} from "react";
+import Counter from "./Components/Counter";
 
 
 
 function App() {
-  const  [count, setCount] = useState(0);
   const  [value, setValue] = useState('Input text');
-
-  function  Increment(){
-    setCount(count + 1);
-  }
-
-  function  Decrement(){
-    setCount(count - 1)
-  }
 
   return (
     <div className="App">
-      <h1>{count}</h1>
-        <h1>{value}</h1>
-        <input
-            type="Input text"
-            value={value}
-            onChange={event => setValue(event.target.value)}
-        />
-      <h1></h1>
-      <button onClick={Increment}>Increment</button>
-      <button onClick={Decrement}>Decrement</button>
-
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
