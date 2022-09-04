dcfdb2a31f7b5c0043930d1ecb0682a5bf0d192f
Dmitro <dimamar1003@gmail.com>
2022-09-04 20:16:50 +0100

    Dayle work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React, {useState} from "react";
-import Counter from "./Components/Counter";
-
+import React, {useRef, useState} from "react";
+import './Stiles/App.css';
+import PostList from "./Components/PostList";
+import PostForm from "./Components/PostForm";
 
 
 function App() {
-  const  [value, setValue] = useState('Input text');
+  const [posts,setPosts] = useState([
+    {id: 1, title: 'Java', body: 'Java'},
+    {id: 2, title: 'Java', body: 'is'},
+    {id: 3, title: 'Java', body: 'programming'},
+    {id: 4, title: 'Java', body: 'language'}
+  ])
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
 
   return (
-    <div className="App">
-      <Counter/>
-      <Counter/>
+    <div>
+      <PostForm create = {createPost} />
+      {posts.length !== 0
+          ?
+          <PostList remove = {removePost} posts={posts} title="Posts List"/>
+          :
+          <h1 style={{textAlign: "center"}}>
+            Posts list is empty
+          </h1>
+      }
     </div>
   );
 }
